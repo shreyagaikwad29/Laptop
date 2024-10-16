c8a1316d59cda9c75718f6a172b825992e94b4ac
shreyagaikwad29 <shreyag2953@gmail.com>
2024-10-16 18:14:17 +0530

    changes in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Home from "./Component/Home";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Component/Navbar';
 import "./App.css"
@@ -8,7 +9,8 @@ const App=()=> {
     <>
        <Navbar/>
        <Routes>
-            <Route index element={<Home/>}/>
+            <Route path="/" element={<Auth/>}/>
+            <Route path="/Home" element={<Home/>}/>
        </Routes>
     </>
   )
