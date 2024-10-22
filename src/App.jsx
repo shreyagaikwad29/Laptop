901bb5feb77bdcfecc4395e608e4d4f0d9ec401a
shreyagaikwad29 <shreyag2953@gmail.com>
2024-10-22 10:18:41 +0530

    New layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Auth from "./pages/Auth/SignUp";
 import { Route, Routes } from 'react-router-dom';
 import Navbar from "./pages/Navbar/Navbar";
 import "./App.css"
-import About from './pages/About/About';
 import Product from './pages/Product/Product'
 import Contact from './pages/Contact/Contact';
 import Login from './pages/Auth/Login';
 import SignUp from './pages/Auth/SignUp';
 import UserInfo from './pages/Auth/UserInfo';
+import About from './pages/About/About';
 
 const App=()=> {
   return (
